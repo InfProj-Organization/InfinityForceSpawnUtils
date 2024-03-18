e67d14d2495c56eb87492000647720963d64721f
OldSerpskiStalker <ictopiar@gmail.com>
2024-03-18 20:52:28 +0500

    Template

--- CopyGameWorld/src/main/java/org/example/Main.java
+++ CopyGameWorld/src/main/java/org/example/Main.java
@@ -1,7 +1,39 @@
 package org.example;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args)
+    {
+        
+
+    }
+
+    private static void copyFolder(File sourceFolder, File destinationFolder) throws IOException
+    {
+
+    }
+
+    private static void logInfo(String message)
+    {
+        logger.log(Level.INFO, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " - " + message);
+    }
+
+    private static void logError(String message)
+    {
+        logger.log(Level.SEVERE, LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " - " + message);
     }
 }
